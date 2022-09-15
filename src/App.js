ba46e0f161ad7c7a8cc0139efdd151b4015ee93d
Balagobinath Vellingiri <112892259+balavellingiri@users.noreply.github.com>
2022-09-15 13:14:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React feature1
+          Learn React feature2
         </a>
       </header>
     </div>
