45cebdb76889119429fcdf2b2bf4cd194dccdcea
Balagobinath Vellingiri <112892259+balavellingiri@users.noreply.github.com>
2022-09-16 09:10:26 -0400

    feature 3

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React feature2
+          Learn React feature3
         </a>
       </header>
     </div>
